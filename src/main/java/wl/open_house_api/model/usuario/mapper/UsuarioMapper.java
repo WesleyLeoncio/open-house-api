51990c9294a4da7b24308073e85dd3753cd20181
Wesley Cruz <wesley.leoncio.izi@gmail.com>
2023-07-20 11:51:21 -0300

    Refatorando teste

--- src/main/java/wl/open_house_api/model/usuario/mapper/UsuarioMapper.java
+++ src/main/java/wl/open_house_api/model/usuario/mapper/UsuarioMapper.java
@@ -5,7 +5,6 @@ import org.mapstruct.factory.Mappers;
 import wl.open_house_api.model.usuario.entity.Usuario;
 import wl.open_house_api.model.usuario.request.UsuarioRequestCreatMaster;
 import wl.open_house_api.model.usuario.request.UsuarioRequestCreatUser;
-import wl.open_house_api.model.usuario.request.UsuarioRequestEditMaster;
 import wl.open_house_api.model.usuario.response.UsuarioResponse;
 import wl.open_house_api.model.usuario.response.UsuarioResponseCrud;
 
--- src/test/java/wl/open_house_api/model/usuario/mapper/UsuarioMapperTest.java
+++ src/test/java/wl/open_house_api/model/usuario/mapper/UsuarioMapperTest.java
@@ -1,27 +1,38 @@
 package wl.open_house_api.model.usuario.mapper;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import wl.open_house_api.model.profile.request.ProfileRequest;
+import wl.open_house_api.model.profile.request.ProfileRequestRole;
+import wl.open_house_api.model.usuario.entity.Usuario;
+import wl.open_house_api.model.usuario.request.UsuarioRequestCreatMaster;
+import wl.open_house_api.model.usuario.request.UsuarioRequestCreatUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UsuarioMapperTest {
 
-//    @Test
-//    @DisplayName("Deveria converter um UsuarioRequestCreatMaster em uma entity Usuario")
-//    void usuarioResquestCreatMasterToUsuarioCenario1() {
-//
-//        ProfileRequest profileEsperado = new ProfileRequest(1L, "ROLE_MASTER");
-//
-//        UsuarioRequestCreatMaster master = new UsuarioRequestCreatMaster(
-//              "Wesley", "admin@email.com", "123456",profileEsperado
-//        );
-//
-//
-//        Usuario usuario =  UsuarioMapper.INSTANCE.usuarioResquestCreatMasterToUsuario(master);
-//
-//        assertThat( usuario ).isNotNull();
-//        assertThat( usuario.getNome()).isEqualTo( "Wesley" );
-//        assertThat( usuario.getLogin()).isEqualTo( "admin@email.com" );
-//        assertThat( usuario.getSenha()).isEqualTo( "123456" );
-//        assertThat( usuario.getProfile().getNome()).isEqualTo( profileEsperado.nome() );
-//        assertThat( usuario.getProfile().getId()).isEqualTo( profileEsperado.id() );
-//    }
+    @Test
+    @DisplayName("Deveria converter um usuarioResquestCreatUserToUsuario em uma entity Usuario")
+    void usuarioResquestCreatUserToUsuario() {
+        ProfileRequestRole role1 = new ProfileRequestRole(1L);
+        List<ProfileRequestRole> roles = new ArrayList<>();
+        roles.add(role1);
+
+        UsuarioRequestCreatUser user = new UsuarioRequestCreatUser(
+                "Wesley", "admin@email.com", "123456"
+        );
+
+        Usuario usuario =  UsuarioMapper.INSTANCE.usuarioResquestCreatUserToUsuario(user);
+        assertThat( usuario ).isNotNull();
+        assertThat( usuario.getNome()).isEqualTo( "Wesley" );
+        assertThat( usuario.getLogin()).isEqualTo( "admin@email.com" );
+        assertThat( usuario.getSenha()).isEqualTo( "123456" );
+    }
+
 }
+
+
